a52428695e8050bc138aec5676e1aee377a81782
Eric Weiss <ew@phys.au.dk>
2018-03-21 12:57:06 +0000

    main renamed to gpsclockGui

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # picture
 Code for the PICTURE absolute time calibration unit
+The gpsClock Gui is used to control the Gps Pulser and configure time and pulselengt.
+configuration can be done with the GUI, that is started on boot or cmdline thats accessible by login.
+
--- gpsclock.py
+++ gpsclock.py
@@ -1,160 +1,191 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # ex:set noet:
-
+# gpsclok: a tool to configure the gps recevier clock.
+# written by:
+#  rasmus handberg rasmush@phys.au.dk
+# eric weiss            ew@phys.au.dk
+# usage: see Help output
 from __future__ import division, print_function, with_statement
 import serial
 import io
 import logging
 import argparse
+try:
+    import configparser as ConfigParser
+except:
+    import ConfigParser
 from datetime import datetime
 
 class GPSClock(object):
 
-	FAULTS = {
-		1: 'Drift Limit Fault',
-		2: 'Reference Fault',
-		4: 'GPS Communication Fault',
-		8: 'GPS Antenna Fault',
-		16: 'Oscillator Control Fault',
-		32: 'GPS Battery Fault'
-	}
+    FAULTS = {
+            1: 'Drift Limit Fault',
+            2: 'Reference Fault',
+            4: 'GPS Communication Fault',
+            8: 'GPS Antenna Fault',
+            16: 'Oscillator Control Fault',
+            32: 'GPS Battery Fault'
+    }
 
-	def __init__(self, port):
-		# Start logger:
-		self.logger = logging.getLogger(__name__)
-		self.logger.debug('Setting up serial connection')
+    def __init__(self, port):
+        # Start logger:
+        self.logger = logging.getLogger(__name__)
+        self.logger.debug('Setting up serial connection')
+        # Sete Config Parameter
+#                self.config=config
 
-		# Open serial connection to clock:
-		self.ser = serial.Serial(port=port, baudrate=9600, parity=serial.PARITY_NONE, timeout=1.0)
-		self.io = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser, 1),
-                               newline = '\n',
-                               line_buffering = True)
+        # Open serial connection to clock:
+        self.ser = serial.Serial(port=port, baudrate=9600, parity=serial.PARITY_NONE, timeout=1.0)
+        self.io = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser, 1),
+                       newline = '\n',
+                       line_buffering = True)
 
-		# Make sure we are using the right settings:
-		self.SendAndRecieve('ECHO,0') # Turn echoing of commands off
-		self.SendAndRecieve('TMODE,0') # 0=UTC, 1=GPS, 2=LOCAL
-		self.SendAndRecieve('DSTSTATE,0') # Disable Daylight Savings Time
-		self.SendAndRecieve('PPMODE,0') # Disable Programmable Pulse
+        # Make sure we are using the right settings:
+        self.SendAndRecieve('ECHO,0') # Turn echoing of commands off
+        self.SendAndRecieve('TMODE,0') # 0=UTC, 1=GPS, 2=LOCAL
+        self.SendAndRecieve('DSTSTATE,0') # Disable Daylight Savings Time
+        self.SendAndRecieve('PPMODE,0') # Disable Programmable Pulse
 
-		# Check for faults:
-		faults = self.faults
-		if faults is not None:
-			self.logger.error("Faults: %s", faults)
+        # Check for faults:
+        faults = self.faults
+        if faults is not None:
+            self.logger.error("Faults: %s", faults)
 
-		self.logger.debug("init done")
+        self.logger.debug("init done")
 
-	def close(self):
-		self.ser.close()
+    def close(self):
+        self.ser.close()
 
-	def __enter__(self, *args, **kwargs):
-		return self
+    def __enter__(self, *args, **kwargs):
+        return self
 
-	def __exit__(self, *args, **kwargs):
-		self.close()
+    def __exit__(self, *args, **kwargs):
+        self.close()
 
-	def SendAndRecieve(self, cmd):
-		self.logger.debug("Sending '%s'", cmd)
-		self.io.write(cmd + '\r\n')
-		self.ser.flush()
+    def SendAndRecieve(self, cmd):
+        self.logger.debug("Sending '%s'", cmd)
+        self.io.write(cmd + '\r\n')
+        self.ser.flush()
 
-		# Receive an answer:
-		res = self.io.readline()
-		res = res.strip()
-		if res == 'Syntax Error':
-			self.logger.error("Syntax Error: '%s'", cmd)
-			return None
-		self.logger.debug("Received '%s'" % res)
-		if res == '':
-			return None
-		elif cmd == '*': # Special case for "TIME"
-			return res[5:]
-		elif res.startswith(cmd + ','):
-			return res[len(cmd)+1:]
-		else:
-			return res
+        # Receive an answer:
+        res = self.io.readline()
+        res = res.strip()
+        if res == 'Syntax Error':
+            self.logger.error("Syntax Error: '%s'", cmd)
+            return None
+        self.logger.debug("Received '%s'" % res)
+        if res == '':
+            return None
+        elif cmd == '*': # Special case for "TIME"
+            return res[5:]
+        elif res.startswith(cmd + ','):
+            return res[len(cmd)+1:]
+        else:
+            return res
 
-	@property
-	def position(self):
-		"""Current position."""
-		return self.SendAndRecieve('POSITION')
+    @property
+    def position(self):
+        """Current position."""
+        return self.SendAndRecieve('POSITION')
 
-	@property
-	def time(self):
-		"""Current UTC time."""
-		ts = self.SendAndRecieve('*')
-		return datetime.strptime(ts[3:-2], '%Y,%j,%H,%M,%S.%f')
+    @property
+    def time(self):
+        """Current UTC time."""
+        ts = self.SendAndRecieve('*')
+        return datetime.strptime(ts[3:-2], '%Y,%j,%H,%M,%S.%f')
 
-	@property
-	def version(self):
-		"""Version of GPC Clock."""
-		return self.SendAndRecieve('VERSION')
+    @property
+    def version(self):
+        """Version of GPC Clock."""
+        return self.SendAndRecieve('VERSION')
 
-	@property
-	def faults(self):
-		"""List of current faults."""
-		flts = self.SendAndRecieve('FLTS')
-		flts = int(flts)
-		if flts == 0:
-			return None
-		# There was an error, figure out which ones:
-		faults = []
-		for bitval, description in self.FAULTS.items():
-			if flts & bitval > 0:
-				faults.append(description)
-		return faults
+    @property
+    def faults(self):
+        """List of current faults."""
+        flts = self.SendAndRecieve('FLTS')
+        flts = int(flts)
+        if flts == 0:
+            return None
+        # There was an error, figure out which ones:
+        faults = []
+        for bitval, description in self.FAULTS.items():
+            if flts & bitval > 0:
+                faults.append(description)
+        return faults
 
-	def setPulseTime(self, time=None, pps=True, autostart=True):
-		# Set the time where pulse should be sent:
-		# TODO: This needs to be set
-		if time is not None:
-			self.SendAndRecieve('PPTIME,XXX:XX:XX:XX.0000000') # One pulse per second - on the second
-		elif pps:
-			self.SendAndRecieve('PPTIME,XXX:XX:XX:XX.0000000') # One pulse per second - on the second
-		# If specified, also start the pulse:
-		if autostart:
-			self.startPulse()
-			
-	def startPulse(self):
-		self.SendAndRecieve('PPMODE,1')
+    def setPulseTime(self, time=None, pps=True, autostart=True,conf=None):
+        # Set the time where pulse should be sent:
+        # TODO: This needs to be set
+        if time is not None:
+            self.SendAndRecieve('PPTIME,XXX:XX:XX:XX.0000000') # One pulse per second - on the second
+        elif conf is not None:
+            import datetime
+            dt=conf.get('settings','date')
+            tm=conf.get('settings','time')
+            (Year,Month,Day)=dt.split('/')
+            (Hour,Min,Sec)=tm.split(':')
+            a=datetime.datetime(int(Year), int(Month), int(Day), hour=int(Hour), minute=int(Min), second=int(Sec), microsecond=0, tzinfo=None)
+            sendStr="PPTIME,%03i:%02i:%02i:%02i.0000000" % (a.timetuple().tm_yday, a.timetuple().tm_hour,a.timetuple().tm_min,a.timetuple().tm_sec )
+            self.SendAndRecieve(sendStr) # One pulse per second - on the second
+        # If specified, also start the pulse:
+        elif pps:
+            self.SendAndRecieve('PPTIME,XXX:XX:XX:XX.0000000') # One pulse per second - on the second
 
-	def stopPulse(self):
-		self.SendAndRecieve('PPMODE,0')
+        if autostart:
+            self.startPulse()
+
+    def startPulse(self):
+        self.SendAndRecieve('PPMODE,1')
+
+    def stopPulse(self):
+        self.SendAndRecieve('PPMODE,0')
 
 if __name__ == '__main__':
 
-	logging_level = logging.INFO
+    logging_level = logging.INFO
+    ConfigFile='config.ini'
 
-	# Command-line input:
-	parser = argparse.ArgumentParser(description='Configure GPS Clock for PICTURE.')
-	parser.add_argument('-p', '--port', type=str, help='COM port.', default='COM5')
-	parser.add_argument('-d', '--debug', help='Print debug messages.', action='store_true')
-	parser.add_argument('-q', '--quiet', help='Only report warnings and errors.', action='store_true')
-	args = parser.parse_args()
-	port = args.port
-	if args.quiet:
-		logging_level = logging.WARNING
-	elif args.debug:
-		logging_level = logging.DEBUG
+    # Command-line input:
+    parser = argparse.ArgumentParser(description='Configure GPS Clock for PICTURE.')
+    parser.add_argument('-p', '--port', type=str, help='COM port.', default='COM5')
+    parser.add_argument('-d', '--debug', help='Print debug messages.', action='store_true')
+    parser.add_argument('-q', '--quiet', help='Only report warnings and errors.', action='store_true')
+    parser.add_argument('-c', '--config',type=str , help='Use settings based of Config File.', default="config.ini")
+    args = parser.parse_args()
+    port = args.port
 
-	# Setup logging:
-	logger = logging.getLogger(__name__)
-	logger.setLevel(logging_level)
-	if not logger.hasHandlers():
-		formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-		console = logging.StreamHandler()
-		console.setFormatter(formatter)
-		logger.addHandler(console)
 
-	# Do something simple:
-	print("Starting...")
-	print("COM Port: ", port)
-	with GPSClock(port) as c:
-		print("Version:  ", c.version)
-		print("Time:     ", c.time)
-		print("Position: ", c.position)
-		print("Faults:   ", c.faults)
+    if args.quiet:
+        logging_level = logging.WARNING
+    elif args.debug:
+        logging_level = logging.DEBUG
 
-		c.setPulseTime()
+    if args.config:
+        config = ConfigParser.ConfigParser()
+        config.read_file(open(args.config))
+    else:
+        config=None
 
-	print("Done.")
+    # Setup logging:
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging_level)
+    if not logger.hasHandlers():
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        console = logging.StreamHandler()
+        console.setFormatter(formatter)
+        logger.addHandler(console)
+
+    # Do something simple:
+#    print("Starting...")
+#    print("COM Port: ", port)
+    with GPSClock(port) as c:
+#        print("Version:  ", c.version)
+#        print("Time:     ", c.time)
+#        print("Position: ", c.position)
+#        print("Faults:   ", c.faults)
+
+        c.setPulseTime(conf=config)
+
+#    print("Done.")
